579c16cd7eccb60fe9763fd06287d50dbe3c3321
Bisakh Mondal <bisakhmondal00@gmail.com>
2020-09-12 19:54:28 +0530

    mongo client

---
+++ serving-api/data/mongoClient.go
@@ -0,0 +1,20 @@
+package data
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"context"
+)
+
+//MongoClient for Database handling.
+type MongoClient struct{
+	ctx *context.Context
+	client *mongo.Client
+}
+
+// New Client initialization.
+func NewMongoClient(ct *context.Context, clt *mongo.Client) *MongoClient{
+	return &MongoClient{
+		ctx: ct,
+		client: clt,
+	}
+}
