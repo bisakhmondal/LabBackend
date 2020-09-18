cf1caeb6baeb97a9e55c415b58d9c2725926d35b
theSYNcoder <papaigd@gmail.com>
2020-09-18 20:54:11 +0530

    Add

--- server/server.go
+++ server/server.go
@@ -13,7 +13,7 @@ func New(smux *mux.Router, bindAddress string) *http.Server{
 	//cors
 	// corsH := handlers.CORS(handlers.AllowedOrigins([]string{"*"}))
 
-	corsH := handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))
+	corsH := handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"34.83.188.4","34.83.188.4:3000","127.0.0.1"}))
 
 
 	
